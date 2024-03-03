a0bfb3f034012d5f350da308cdb5392fbdd7b8e4
Felipe Izolan <80170121+FelipeIzolan@users.noreply.github.com>
2024-03-03 03:10:33 -0300

    update

--- src/pages/boot.tsx
+++ src/pages/boot.tsx
@@ -5,30 +5,49 @@ type BootProps = {
   setPage: StateUpdater<'boot' | 'terminal' | 'ui'>;
 };
 
+type Step = [string, number];
+
 const Boot = (props: BootProps) => {
   const [text, setText] = useState<string>('');
 
   useEffect(() => {
-    const steps = [
-      { text: BIOS, delay: 200 },
-      { text: getSpriteImg(), delay: 200 },
-      { text: 'Loading <span class="text-orange">Izo-Kernel</span>...;<br/>', delay: 200 },
-      { text: [logOK('processor'), logOK('input/output'), logOK('memory'), logOK('disk'), logOK('device')].join(''), delay: 50 },
-      { text: 'Loading <span class="text-orange">Daemons</span>...;<br/>', delay: 200 },
-      { text: [logOK('systemd'), logOK('kthread'), logOK('kworker'), logOK('network'), logOK('pipewire'), logOK('dm'), logOK('wm')].join(''), delay: 50 },
-      { text: 'Loading <span class="text-orange">Shell</span>...;<br/>', delay: 200 },
-      { text: [logOK('bash'), logOK('environment')].join(''), delay: 50 },
-      { text: `date: ${new Date().toString()}<br/>screen: ${window.screen.width}x${window.screen.height}<br/>lang: ${window.navigator.language}<br/>`, delay: 50 },
-      { text: 'Opening <span class="text-orange">OS</span>...;<br/>', delay: 200 },
-      { text: logOK('everything is ok!'), delay: 50 }
+    let time = 0;
+    let steps: Step[] = [
+      [BIOS, 100],
+      [getSpriteImg(), 100],
+      ['Loading <span class="text-orange">Izo-Kernel</span>.', 100 ],
+      ['.', 50],
+      ['.;<br/>', 50],
+      [logOK('CPU'), 100],
+      [logOK('GPU'), 100],
+      [logOK('Input/Output'), 50],
+      [logOK('Memory'), 100],
+      [logOK('Disk'), 100],
+      [logOK('Device') + '<br/>', 100],
+      ['Loading <span class="text-orange">Daemons</span>.', 100],
+      ['.', 50],
+      ['.;<br/>', 50],
+      [logOK('systemd'), 100],
+      [logOK('kthread'), 100],
+      [logOK('kworker'), 100],
+      [logOK('network'), 100],
+      [logOK('pipewire'), 100],
+      [logOK('dm'), 50],
+      [logOK('wm') + '<br/>', 50],
+      ['Loading <span class="text-orange">Shell</span>.', 100],
+      ['.', 50],
+      ['.;<br/>', 50],
+      [logOK('bash'), 100],
+      [logOK('environment') + '<br/>', 100],
+      [`date: ${new Date().toString()}<br/>screen: ${window.screen.width}x${window.screen.height}<br/>lang: ${window.navigator.language}<br/><br/>`, 50],
+      ["Hello, World!", 100],
     ];
 
-    let time = 0;
-    steps.forEach(step => {
-      setTimeout(() => setText(prevText => prevText + step.text), time += step.delay);
-    });
+    for (let [text, delay] of steps)
+      setTimeout(() => setText(prevText => prevText + text), time += delay);
 
-    setTimeout(() => props.setPage('terminal'), time + 850);
+    setTimeout(() => props.setPage('terminal'), time + 700);
+    // time == 3000;
   }, []);
 
   return <p dangerouslySetInnerHTML={{ __html: text }} className='ml-10 leading-3'/>;
